ab381caacfe68fa7c3ccf58445e596046abaac6e
Mei Qi Ng <meiqi.ng39@spsmail.cuny.edu>
2026-02-24 21:58:07 -0500

    Refactor chess tournament data processing and export
    Transformed chess tournament results from a text file into a structured CSV file suitable for SQL database usage. The project involved data loading, tidying, calculating averages, and exporting the final dataset.

--- Project_1/Project1_Chess_codebase.R
+++ Project_1/Project1_Chess_codebase.R
@@ -1,13 +1,3 @@
----
-  title: "Project 1"
-author: "Mei Qi Ng"
-date: "`r Sys.Date()`"
-output: html_document
----
-  
-  ```{r setup, include=FALSE}
-knitr::opts_chunk$set(echo = TRUE)
-```
 
 ## [**Approach**]{.underline}
 
@@ -17,10 +7,6 @@ In this project, we are given a text file with chess tournament results where in
   
   Starting this there was the challenge of trying to tidy this data set as there are many pipelines and spaces that are in the way as well as trying to extract information to creating columns with the correct names and their corresponding data. Using strings and regular expression often was necessary for me to properly organize the data.
 
-```{r echo=FALSE, out.width="40%", fig.align="center"}
-knitr::include_graphics("https://images.pexels.com/photos/277092/pexels-photo-277092.jpeg")
-
-```
 
 (1) Data Loading: The tournament text file will be posted on Github, which allows me to load the data frame.
 
@@ -36,13 +22,12 @@ Challenges: Based on looking at raw tournament txt file, I see that there are so
 
 (1) Gemini Pro - to troubleshoot regular expressions for extracting the opponents ratings.
 
--   *AI citation -(Google DeepMind. (2026). Gemini Pro [Large language model]. <https://gemini.google.com>. Accessed February 19, 2026*
+ *AI citation -(Google DeepMind. (2026). Gemini Pro [Large language model]. <https://gemini.google.com>. Accessed February 19, 2026*
                      
                      (2) [Posit Stringr/Regrex cheatsheet](https://posit.co/wp-content/uploads/2022/10/strings-1.pdf)
                    
                    (3) R for Data Science 2e : Chapters from Week 3
                    
-                   ```{r}
                    # Load data
                    
                    library(tidyverse)
@@ -50,9 +35,9 @@ Challenges: Based on looking at raw tournament txt file, I see that there are so
                    raw_data <- read.csv("https://raw.githubusercontent.com/meiqing39/DATA-607/refs/heads/main/Project_1/tournamentinfo.txt")
                    
                    head(raw_data)
-                   ```
-                   
-                   ```{r}
+        
+              
+
                    #remove first 2 rows of non data
                    
                    new_df <- raw_data |> 
@@ -62,9 +47,7 @@ Challenges: Based on looking at raw tournament txt file, I see that there are so
                    
                    head(new_df)
                    
-                   ```
-                   
-                   ```{r}
+
                    # Data Tidying 
                    
                    #Split into 2 tables: odd vs even rows ( Name & Matches vs Staes & Rating)\
@@ -80,9 +63,9 @@ Challenges: Based on looking at raw tournament txt file, I see that there are so
                    
                    head(r2_data, 4)
                    
-                   ```
+
                    
-                   ```{r}
+
                    #create columns with |
                    
                    sep1_data <- r1_data |> 
@@ -95,9 +78,9 @@ Challenges: Based on looking at raw tournament txt file, I see that there are so
                      separate_wider_delim( cols = raw_text, 
                                            delim = "|", names = c("Player_State", "Rating_Info", NA, NA, NA, NA, NA, NA, NA, NA, NA)) 
                    # fill in empty columns with NA to run this function
-                   ```
+
                    
-                   ```{r}
+
                    # CLEAN AND EXTRACT TARGET DATA
                    
                    clean_sep1 <- sep1_data |>  
@@ -140,42 +123,13 @@ Challenges: Based on looking at raw tournament txt file, I see that there are so
                    
                    opponent_averages
                    
-                   ```
-                   
-                   # **Hand Calculation**
-                   
-                   [1] Player who played all games: Player 3 ADITYA BAJAJ
-                   
-                   ```         
-                   Average Opponent's Pre Rating = (1641+955+1745+1563+1712+1666+1663)/7 = 1563.5 = 1534
-    
-```
 
-| Player Number each played round | Pre Ratings |
-|---------------------------------|-------------|
-| 8                               | 1641        |
-| 61                              | 955         |
-| 25                              | 1745        |
-| 21                              | 1563        |
-| 11                              | 1712        |
-| 13                              | 1666        |
-| 12                              | 1663        |
-
-[2] Player who played less than all games: Player 53 JOSE C YBARRA
-
-```         
-Average Opponent's Pre Rating = (1745+1199+1092)/3 = 1345.33 = 1345
-                   ```
                    
-                   | Player number of each played rounds | Pre ratings |
-                     |-------------------------------------|-------------|
-                     | 25                                  | 1745        |
-                     | 44                                  | 1199        |
-                     | 57                                  | 1092        |
-                     
+                   
+
+
                      # **Finalize and Export to CSV**
                      
-                     ```{r}
                    # Join averages column to main table and select the 5 columns
                    
                    final_project_data <- combined_data |> 
@@ -189,12 +143,5 @@ Average Opponent's Pre Rating = (1745+1199+1092)/3 = 1345.33 = 1345
                    # Export data set to a CSV file
                    
                    write.csv(final_project_data, "project1_chess_results.csv", row.names = FALSE)
-                   ```
-                   
-                   # **Conclusion**
-                   
-                   I transformed chess tournament chess result from a text file into a formatted CSV file suitable for SQL relational database usage. The challenge of this data set was the many multi-row layout. Chess players would have a distinct row, however the many white spaces, delimiters, and dashed lines took many trial and errors. By using R codes such as regular expressions (stringr) for pattern extraction, tidyr functions (separate_wider_delim) and pivot_longer, I was able to clean, separate and restructure the table into an organized format.
-                   
-                   After, I calculated the average pre-rating of opponents using smaller tables/combine tables which shows the relational data based connection in SQL. Extracting opponent's ID in match history then joining them with isolated pre rating numbers and sorting out the un-played games and byes, helped in creating the final metrics. These programmed results are then verified against manual hand calculated result to check for accuracy.
 
-In conclusion, this project demonstrated the value of R data cleaning. To manually edit the text file would have taken more time and keen eye which leaves room for mistakes. Developing this R script provides a automated and reproducible file that can quickly process same formatted future tournament information and can be used in SQL for a analysis ready table.
+                  
